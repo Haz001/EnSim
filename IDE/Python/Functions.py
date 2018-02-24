b5be5de5c4dca5a7c337bc33f749ef8c0537e3d5
Haz <master@hazonline.co.uk>
2018-02-24 22:24:41 +0000

    Add files via upload

--- IDE/Python/Functions.py
+++ IDE/Python/Functions.py
@@ -2,11 +2,12 @@ class console:#class of console
     c_show_var = False;#varible for console show
     def show(cmd1):#show comand
 ##        try:
-            if (str(cmd1) == "get"):
+            if (str(cmd1).lower() == "get"):
 ##                print("Get")
                 return console.c_show_var;                
             else:
-                console.c_show_var = bool(cmd1);
+                
+                console.c_show_var = bool(str(cmd1).capitalize());
 ##                print("Set")
 ##        except:
 ##            print("Error");
